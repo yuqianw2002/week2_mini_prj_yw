4036f67091357f96aef725ad595f3c9481c82149
yuqianw2002 <wang.yuqian2@northeastern.edu>
2025-09-08 15:45:57 -0400

    Update data.py

--- data.py
+++ data.py
@@ -1,4 +1,13 @@
 import pandas as pd
 
-df = pd.read_csv("boston_weather_data.csv")
-print(df)
+# read data using df
+bos = pd.read_csv("boston_weather_data.csv")
+
+# inspect dataset, the summary stats and data type
+print(f"{bos.shape[0]} rows × {bos.shape[1]} columns")
+print(f"First 10 columns:\n{bos.head()}")
+print(f'Info: \n{bos.info()}')
+print(f'Summary stats{bos.describe()}')
+print(f"Missing value of each column:\n{bos.isna().sum()}")
+
+# 
