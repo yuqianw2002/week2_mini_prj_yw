7c631a064e9a9ca0c84254c3b61b70baae45080f
yuqianw2002 <wang.yuqian2@northeastern.edu>
2025-09-08 21:47:26 -0400

    update data.py

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
-# Week2_mini_prj_yw
+# Week2_mini_prj_yw - Analysis Boston weather from 2013 to 2023
 
 - A data anaylsis for the Boston weather from 2013 to 2023 dataset from Kaggle (https://www.kaggle.com/datasets/swaroopmeher/boston-weather-2013-2023/data)
 - Import data by Pandas, inspecting, filter, grouping, visulizing and exploring with a simple ML algorithm. 
 
 ## Goal
-The goal of this data anaylsis is compare the 10 years Boston weather from 2013 to 2023, and try to predict the future weather. 
+The goal of this data anaylsis is compare the 10 years Boston weather from 2013 to 2023, and try to predict the future weather. After group the data with each month, then filter the winter monthes which from 2013 to 2023 to compare the differences of all weather data. To indicate of the trend of winter. Use linear regression model on the whole data to predict future temperature. The scatter plot have the red dot as the predition temperature, and blue is the real tempearture. X aixs is Wind speed and Y axis is temperature.  
 
 
 ## This dataset contains the following columns:
--- data.py
+++ data.py
@@ -1,5 +1,10 @@
 import pandas as pd
 import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 # read data using df
 bos = pd.read_csv("boston_weather_data.csv")
@@ -21,9 +26,51 @@ bos = bos.drop('time', axis=1)
 
 # group by each year and month, and take mean of columns except date data
 monthly_mean = (
-    bos.groupby(["Year", "Month"], as_index=False)
-      .mean(numeric_only=True)    
+    bos.groupby(["Year", "Month"], as_index=False).mean()  
       .sort_values(["Year", "Month"])
 )
+print(monthly_mean.head())
 
-print(monthly_mean.head(12))
+# filtering Winter month (12-2), get mean value of winter month
+winter_df = monthly_mean[monthly_mean["Month"].isin([12, 1, 2])]
+winter_mean = (winter_df.groupby(["Year"], as_index=False).mean().sort_values(["Year"]))
+print(winter_mean.drop('Month', axis=1))
+
+# linear regression on the data
+mdl = LinearRegression()
+
+# impute missing value with mean of the col
+num_cols = bos.select_dtypes(include='number').columns
+bos[num_cols] = bos[num_cols].fillna(bos[num_cols].mean())
+print(bos.isnull().sum())
+
+# select features 
+# features = ['prcp', 'wdir', 'wspd', 'pres']
+features = ['wspd', 'pres', 'wdir']
+target = ['tavg']
+X = bos[features]
+y = bos[target]
+
+# split data into 20% test size
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+# fit the mdl
+mdl.fit(X_train, y_train)
+
+# predict test value
+y_pred = mdl.predict(X_test)
+# y_pred = pd.DataFrame(y_pred)
+
+# evaluate model
+mse = mean_squared_error(y_pred, y_test)
+print("The mean squared error:", mse)
+
+# plot the true value and pred value
+plt.scatter(X_test.iloc[:,0], y_test, color='blue', label='Actual')
+plt.scatter(X_test.iloc[:,0], y_pred, color='red', label='Prediction')
+plt.title("Actual temperature vs. Prediction temperaturte Boston winter")
+plt.xlabel('Wind speed')
+plt.ylabel('Temperature')
+plt.legend()
+plt.savefig('test_pred.png')
+plt.show()
